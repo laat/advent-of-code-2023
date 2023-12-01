3fa7e4482e98025fa29b0402e479832a0622a531
Sigurd Fosseng <sigurd.fosseng@nrk.no>
2023-12-01 12:05:15 +0100

    day1

--- day1.clj
+++ day1.clj
@@ -3,13 +3,32 @@
    [aocd.core :as data]
    [clojure.string :as str]))
 
-(def example "")
-(def input (data/input 2022 1))
+(def example "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen")
+(def input (data/input 2023 1))
 
 ;; part1
-(->> example
-     str/split-lines)
+(->> input
+     str/split-lines
+     (map (partial re-seq #"\d"))
+     (map (partial map parse-long))
+     (map (fn [xs] (+ (* (first xs) 10) (last xs))))
+     (reduce +))
+
 
 ;; part2
-(->> example
-     str/split-lines)
+(def numbers {"one" 1 "two" 2 "three" 3 "four" 4 "five" 5
+              "six" 6 "seven" 7 "eight" 8 "nine" 9
+              "1" 1 "2" 2 "3" 3 "4" 4 "5" 5 "6" 6 "7" 7 "8" 8 "9" 9})
+(->> input
+     str/split-lines
+     (map (partial re-seq #"(?=(one|two|three|four|five|six|seven|eight|nine|[0-9]))"))
+     (map (partial map second))
+     (map (partial map numbers))
+     (map (fn [xs] (+ (* (first xs) 10) (last xs))))
+     (reduce +))
