eef1856da0d865e191ba5d21547b4b6ef73b4892
Sigurd Fosseng <sigurd.fosseng@nrk.no>
2023-12-04 11:55:00 +0100

    day2

---
+++ day2.clj
@@ -0,0 +1,39 @@
+(ns day2
+  (:require
+   [aocd.core :as data]
+   [clojure.string :as str]))
+
+(def input (data/input 2023 2))
+
+(defn parse-cubes [cubes]
+  (->> (re-seq #"(\d+) (\w+)" cubes)
+       (map (fn [[_ v k]] [(keyword k) (parse-long v)]))
+       (into {})))
+
+(defn parse-line [index line]
+  (->> (str/split (second (str/split line #": ")) #"; ")
+       ;; (map #(str/split % #", "))
+       (mapv parse-cubes)
+       (assoc {:game (+ index 1)} :bags)))
+
+(defn is-possible? [bag]
+  (and (<= (get bag :red 0) 12)
+       (<= (get bag :green 0) 13)
+       (<= (get bag :blue 0) 14)))
+
+;; part1
+(->> input
+     str/split-lines
+     (map-indexed parse-line)
+     (filter #(every? is-possible? (:bags %)))
+     (map :game)
+     (reduce +))
+
+;; part2
+(->> input
+     str/split-lines
+     (map-indexed parse-line)
+     (map :bags)
+     (map (partial apply merge-with max))
+     (map (partial reduce (fn [prod [_ v]] (* prod v)) 1))
+     (reduce +))
